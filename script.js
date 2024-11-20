fc191d876396759a851ec64b4ce1e81164cdc0a5
Fabiano Gomes <fabianogfandrade@gmail.com>
2024-11-19 21:08:08 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // Pegar a tag img
+    const img = document.querySelector('#profile img')
+
+    // Verificar se está no modo light
+    if(html.classList.contains('light')) {
+        // Se tiver lightmode, add img light
+        img.setAttribute('src', '/assets/avatar-light.png')
+    } else {
+        // Se tiver sem light mode, manter a imagem normal
+        img.setAttribute('src', '/assets/avatar.png')
+    }
+}
